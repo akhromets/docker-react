0ba7565cc15cf179fcafda2e7eadfd621a2d8715
Alexey Khromets <akhromets@webinerds.com>
2020-02-13 12:02:27 +0200

    revert app text to pass tests

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changed code from feature branch
+          Learn react
         </a>
       </header>
     </div>
